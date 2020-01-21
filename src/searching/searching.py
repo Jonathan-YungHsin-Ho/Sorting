4918619b22cb2fc75addbc7c49f6ae5fbf947b07
Jonathan Ho <johnnyho82@gmail.com>
2020-01-20 19:26:21 -0500

    Complete binary_search Stretch Goal

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -18,7 +18,14 @@ def binary_search(arr, target):
     high = len(arr)-1
 
     # TO-DO: add missing code
-
+    while low <= high:
+        middle = (low + high) // 2
+        if target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+        else:
+            return middle
     return -1  # not found
 
 
